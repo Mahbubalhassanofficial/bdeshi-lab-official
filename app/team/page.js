c7f1f80b1ed17e84f0c95e04c710d40b69df710a
Mahbubalhassanofficial <90603087+Mahbubalhassanofficial@users.noreply.github.com>
2025-11-27 16:41:33 +0700

    Update page.js

--- app/team/page.js
+++ app/team/page.js
@@ -1,7 +1,5 @@
-"use client";
-
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 export default function TeamPage() {
   const founder = {
