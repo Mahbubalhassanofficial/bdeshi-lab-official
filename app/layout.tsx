8a9cd5bac6af5d58963ffcdb5201440d19bcfd21
Mahbubalhassanofficial <90603087+Mahbubalhassanofficial@users.noreply.github.com>
2025-11-27 14:54:29 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "B'Deshi Emerging Research Lab",
+  description: "Building Smarter, Safer, and More Sustainable Mobility Systems with Artificial Intelligence",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
 
